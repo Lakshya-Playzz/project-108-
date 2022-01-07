41ad930433609c7b1d9189bf4aecfdaf6b699c63
Lakshya-Playzz <85876093+Lakshya-Playzz@users.noreply.github.com>
2022-01-07 13:37:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+//https://teachablemachine.withgoogle.com/models/el59pzWgL/
+function startclassification(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/el59pzWgL/model.json" , modelReady)
+
+}
+function modelReady(){
+    console.log("modelReady");
+    classifier.classify(gotResult)
+
+}
+function gotResult(error,result){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(result);
+        var soundName = result[0].label
+        var confidence = (result[0].confidence*100).toFixed(2);
+        r = Math.floor(Math.random()*255)+1
+        g = Math.floor(Math.random()*255)+1
+        b = Math.floor(Math.random()*255)+1
+        document.getElementById("result_label").innerHTML = "I Can Hear-" + soundName
+        document.getElementById("result_confidence").innerHTML = "Accuracy-" + confidence
+        document.getElementById("result_label").style.color = "rgb("+r+","+g+","+b+")"
+        document.getElementById("result_confidence").style.color = "rgb("+r+","+g+","+b+")"
+
+ 
+ if(soundName == "saying cat"){
+     document.getElementById("image1").src = "cat.jpg"
+ }
+ else if(soundName == 'Saying "lion"' ){
+     document.getElementById("image1").src = "lion.jpg"
+ }
+ else if(soundName == 'saying "cow"'){
+     document.getElementById("image1").src = "cow.jpg"
+ }
+ else{
+     document.getElementById("image1").src = "house sparrow.jpeg"
+ }
+ 
+
+    }
+
+
+
+}
